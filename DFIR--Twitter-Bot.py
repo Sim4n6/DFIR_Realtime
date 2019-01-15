20c111ee51df551f5cc9ed647d580c656a7a8666
Sim4n6 <Sim4n6@gmail.com>
2019-01-15 13:33:54 +0000

    set env vars

--- DFIR--Twitter-Bot.py
+++ DFIR--Twitter-Bot.py
@@ -2,8 +2,12 @@ from flask import Flask
 from tweepy import API, OAuthHandler
 
 import pprint
+import os
 
-from keys import consumer_key, consumer_secret, access_token, access_token_secret
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
 
 app = Flask(__name__)
 
