438dee15d110850a3035fa425a87af8e0f2b5939
Sim4n6 <Sim4n6@gmail.com>
2019-01-18 11:17:05 +0000

    set size photo small with auto compress

--- DFIR--Twitter-Bot.py
+++ DFIR--Twitter-Bot.py
@@ -52,7 +52,7 @@ def index():
 
 	# Get URLs  of the random photo to be set
 	bg_photo_urls = bg_photo[0].urls
-	bg_photo_custom_url = bg_photo_urls.raw + "&w=750&dpi=2"
+	bg_photo_custom_url = bg_photo_urls.small + "&w=750&auto=compress"
 
 	return render_template("base.html", tweet=last_tweet.text, line=line, bg_photo=bg_photo_custom_url)
 
