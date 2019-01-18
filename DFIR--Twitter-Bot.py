f3aabd440b8d2400df5941a2d560f09164bd97c3
Sim4n6 <Sim4n6@gmail.com>
2019-01-18 10:09:14 +0000

    set a random photo with a custom width 750 size photo

--- DFIR--Twitter-Bot.py
+++ DFIR--Twitter-Bot.py
@@ -52,9 +52,9 @@ def index():
 
 	# Get URLs  of the random photo to be set
 	bg_photo_urls = bg_photo[0].urls
-	bg_photo_raw_url = bg_photo_urls.raw + "&w=750&dpi=2"
+	bg_photo_custom_url = bg_photo_urls.raw + "&w=750&dpi=2"
 
-	return render_template("base.html", tweet=last_tweet.text, line=line, bg_photo=bg_photo_raw_url)
+	return render_template("base.html", tweet=last_tweet.text, line=line, bg_photo=bg_photo_custom_url)
 
 
 if __name__ == '__main__':
