fc5103e6dd568341e7a3eb35d509b8e46bc839fe
Sim4n6 <Sim4n6@gmail.com>
2019-01-18 09:19:01 +0000

    checking unsplash api auth

--- DFIR--Twitter-Bot.py
+++ DFIR--Twitter-Bot.py
@@ -46,6 +46,7 @@ def index():
 	auth = Auth(client_id, "", "", "")
 	api = Api(auth)
 	json_random_photo = api.photo.random()
+	print(json_random_photo)
 	urls_random_photo = json_random_photo['urls']
 	for url_random_photo in urls_random_photo:
 		print(url_random_photo)
