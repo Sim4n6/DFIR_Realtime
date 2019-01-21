39219f8238cb00d521dfcc9c90935d7c9486e521
Sim4n6 <Sim4n6@gmail.com>
2019-01-21 09:01:00 +0000

    get the whole tweet full text.

--- DFIR--Twitter-Bot.py
+++ DFIR--Twitter-Bot.py
@@ -56,8 +56,8 @@ def index():
 
 	# the tweet
 	the_tweet_status = tweepy_api.get_status(last_tweet.id, tweet_mode='extended')
-	tweet_text = the_tweet_status.text
 	print(dir(the_tweet_status))
+	tweet_text = the_tweet_status.text
 
 	return render_template("base.html", tweet=tweet_text, line=line, bg_photo_url=bg_photo_custom_url)
 
