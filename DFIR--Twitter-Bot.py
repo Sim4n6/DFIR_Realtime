b7b8574a30e611bde21acb79c6e1d1a89ee0a1d2
Sim4n6 <sim4n6@gmail.com>
2019-02-26 18:20:42 +0100

    last 05 tweets backend

--- DFIR--Twitter-Bot.py
+++ DFIR--Twitter-Bot.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, url_for
+import os
+
+from flask import Flask, render_template
+from flask_moment import Moment
 from tweepy import API as tw_API
 from tweepy import OAuthHandler
 from unsplash.api import Api as us_API
 from unsplash.auth import Auth
-from datetime import datetime as dt
 
-import pprint
-import os
+import tweepy
 
 consumer_key = os.environ['consumer_key']
 consumer_secret = os.environ['consumer_secret']
@@ -14,11 +15,11 @@ access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 
 app = Flask(__name__)
+moment = Moment(app)
 
 
 @app.route("/")
 def index():
-
 	# Authentication and access to the API of Tweepy
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
@@ -26,25 +27,15 @@ def index():
 	# Search for the tag "DFIR" and get last_tweet 100 tweets in 01 page
 	tweepy_api = tw_API(auth)
 	tweets_search_4tag = tweepy_api.search("#DFIR", lang='en', rpp=100, tweet_mode="extended")
-	pp = pprint.PrettyPrinter(indent=4)
-	pp.pprint(tweets_search_4tag)
-	pp.pprint(dir(tweepy_api))
 
 	# cursor on the search
-	#for status in tweepy_api.cursor(tweepy_api.search("#DFIR", lang='en', rpp=100, tweet_mode="extended")).items():
-	#	pp.pprint(">>", status)
+	tweets_5 = []
+	for status in tweepy.Cursor(tweepy_api.search, q="#DFIR", lang='en', rpp=100, tweet_mode="extended").items(5):
+		tweets_5.append(status.full_text)
 
 	# get last tweet
 	last_tweet = tweets_search_4tag[0]
 	tweet_text = last_tweet.full_text
-
-	# store id of last_week_tweet in a file
-	with open("last_week_id.txt", 'w') as lst_hd:
-		lst_hd.write(str(last_tweet.id))
-
-	# Read last_week_tweet from a file
-	with open("last_week_id.txt") as lst_hd:
-		line = int(lst_hd.read())
 
 	client_id = os.environ['access_key_unsplash']
 	client_secret = os.environ['secret_key_unsplash']
@@ -62,10 +53,7 @@ def index():
 	bg_photo_urls = bg_photo[0].urls
 	bg_photo_custom_url = bg_photo_urls.raw + "&fit=clamp&h=300&w=350&auto=compress"
 
-	# Get now() datetime
-	now_dt = dt.now()
-
-	return render_template("base.html", tweet=tweet_text, line=line, bg_photo_url=bg_photo_custom_url, date_time=now_dt)
+	return render_template("base.html", tweet=tweet_text, tweets=tweets_5, bg_photo_url=bg_photo_custom_url)
 
 
 if __name__ == '__main__':
